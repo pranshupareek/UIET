d9cd41494fa532013dd05e0ba2f36ad80caeb828
Pranshu Pareek <pranshupareek47@gmail.com>
2019-07-14 02:39:54 +0530

    Added new and changed link color and more

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#login").click(function(){
+       $("#loginModal").modal();
+    });
+    $("#rt").click(function(){
+       $("#rtmodal").modal();
+    });
+});
